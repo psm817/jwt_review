cb52d921a384fb5a166bcc1cf086ad624dd0696f
psm817 <5004jse@gmail.com>
2024-06-15 18:05:38 +0900

    git init

---
+++ src/main/java/com/example/jwt_review/JwtReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwt_review/JwtReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
