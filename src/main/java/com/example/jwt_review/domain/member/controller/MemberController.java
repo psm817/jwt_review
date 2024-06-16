d9c970f2c57260c29b217c786f04d179af6d7d94
psm817 <5004jse@gmail.com>
2024-06-16 16:03:05 +0900

    add RsData

--- src/main/java/com/example/jwt_review/domain/member/controller/MemberController.java
+++ src/main/java/com/example/jwt_review/domain/member/controller/MemberController.java
@@ -2,11 +2,14 @@ package com.example.jwt_review.domain.member.controller;
 
 import com.example.jwt_review.domain.member.entity.Member;
 import com.example.jwt_review.domain.member.service.MemberService;
+import com.example.jwt_review.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,14 +28,24 @@ public class MemberController {
         private String password;
     }
 
+    @Getter
+    @AllArgsConstructor
+    public static class LoginResponse {
+        private final String accessToken;
+    }
+
     @PostMapping("/login")
-    public Member login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
 //        // 테스트용
 //        resp.addHeader("Authentication", "JWT Token");
 
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
         resp.addHeader("Authentication", accessToken);
 
-        return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
+        return RsData.of(
+                "S-1",
+                "access Token 생성",
+                new LoginResponse(accessToken)
+        );
     }
 }
